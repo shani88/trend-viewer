59385d338855022a0132800f6036e1270916d8a5
cjh5414 <cjh5414@gmail.com>
2018-11-29 21:41:28 +0900

    test create time

--- src/main/java/com/naver/hackday/trendviewer/restservice/repository/KeywordRepository.java
+++ src/main/java/com/naver/hackday/trendviewer/restservice/repository/KeywordRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.Date;
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author SaeHan Kim
@@ -14,5 +14,5 @@ import java.util.Set;
  */
 @RepositoryRestResource(collectionResourceRel = "keyword", path = "keyword")
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-    Set<Keyword> findByCreatedTime(@Param("createdTime") Date createdTime);
+    List<Keyword> findByCreatedTime(@Param("createdTime") LocalDateTime createdTime);
 }
