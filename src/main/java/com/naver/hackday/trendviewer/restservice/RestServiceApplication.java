c8a4efbf43b107cdf1dd43d4e22c2adb8b3bc02a
EunmiKang <hv19962000@gmail.com>
2018-11-29 22:37:46 +0900

    EntityScan 어노테이션 추가

--- src/main/java/com/naver/hackday/trendviewer/restservice/RestServiceApplication.java
+++ src/main/java/com/naver/hackday/trendviewer/restservice/RestServiceApplication.java
@@ -2,8 +2,11 @@ package com.naver.hackday.trendviewer.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = { RestServiceApplication.class, Jsr310JpaConverters.class })
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
