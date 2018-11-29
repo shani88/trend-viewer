59c0e684f15e80b3b48cc76cfde614953ba9d922
cjh5414 <cjh5414@gmail.com>
2018-11-29 20:30:42 +0900

    Add keyword repository test

--- src/test/java/com/naver/hackday/trendviewer/restservice/RestServiceApplicationTests.java
+++ src/test/java/com/naver/hackday/trendviewer/restservice/RestServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.naver.hackday.trendviewer.restservice;
 
 
+import com.naver.hackday.trendviewer.restservice.model.Keyword;
 import com.naver.hackday.trendviewer.restservice.model.TestModel;
+import com.naver.hackday.trendviewer.restservice.repository.KeywordRepository;
 import com.naver.hackday.trendviewer.restservice.repository.TestRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,20 +11,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestServiceApplicationTests {
-
-
     @Autowired
     TestRepository repository;
+
+    @Autowired
+    KeywordRepository keywordRepository;
+
 
     @Test
     public void di() {
         assertThat(repository).isNotNull();
-
     }
 
     @Test
@@ -33,6 +39,11 @@ public class RestServiceApplicationTests {
         assertThat(repository.findByNameIsLikeIgnoreCase("%bye%")).isEmpty();
         repository.save(testModel);
         assertThat(repository.findByNameIsLikeIgnoreCase("%bye%")).hasSize(1);
+    }
+
+    public void keywordAPITest() {
+        Set<Keyword> keywordSet = keywordRepository.findByCreatedTime(new Date(2018, 11, 29, 18, 0, 0));
+        assertThat(keywordSet.size()).isEqualTo(1);
     }
 
 }
