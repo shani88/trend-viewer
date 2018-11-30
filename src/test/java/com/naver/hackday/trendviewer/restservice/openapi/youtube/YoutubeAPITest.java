bce12339bd493f105e2c853860456e8ec8494ae9
EunmiKang <hv19962000@gmail.com>
2018-11-30 11:38:20 +0900

    youtubeAPI request test 코드 원래대로 복구

--- src/test/java/com/naver/hackday/trendviewer/restservice/openapi/youtube/YoutubeAPITest.java
+++ src/test/java/com/naver/hackday/trendviewer/restservice/openapi/youtube/YoutubeAPITest.java
@@ -1,21 +1,30 @@
 package com.naver.hackday.trendviewer.restservice.openapi.youtube;
 
 import com.naver.hackday.trendviewer.restservice.openapi.youtube.model.Youtube;
+
 import java.util.List;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Profile("dev")
 public class YoutubeAPITest {
 
-  private YoutubeAPI youtubeAPI = new YoutubeAPI();
+	private YoutubeAPI youtubeAPI = new YoutubeAPI();
 
-  private static final Logger logger = LoggerFactory.getLogger(YoutubeAPITest.class);
-
-  @Test
-  public void youtubeRequestTest() {
-    List<Youtube> youtubeList = youtubeAPI.request("와썹맨", 5);
-    youtubeList.stream().forEach(youtube -> logger.info(youtube.toString()));
-  }
+	private static final Logger logger = LoggerFactory.getLogger(YoutubeAPITest.class);
+	
+	@Test
+	public void youtubeRequestTest() {
+		List<Youtube> youtubeList = youtubeAPI.request("와썹맨", 5);
+		youtubeList.stream().forEach(youtube -> logger.info(youtube.toString()));
+	}
 
 }
